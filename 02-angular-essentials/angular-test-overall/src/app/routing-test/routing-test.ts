61272764d00ecfd2f9fd05079e753a56d4b95ba0
Arkarhein-123 <hein90510@gmail.com>
2026-08-17 22:58:07 +0630

    learn angular router

--- 02-angular-essentials/angular-test-overall/src/app/routing-test/routing-test.ts
+++ 02-angular-essentials/angular-test-overall/src/app/routing-test/routing-test.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from "../../../node_modules/@angular/router/types/_router_module-chunk";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-routing-test',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './routing-test.html',
   styleUrl: './routing-test.css',
 })
-export class RoutingTest {
-
-}
+export class RoutingTest {}
