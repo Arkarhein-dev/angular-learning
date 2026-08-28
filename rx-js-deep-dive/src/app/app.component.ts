ec7ef831ffa116c7a3a3b04670a3a52bf00f4c2d
Arkarhein-dev <hein90510@gmail.com>
2026-08-28 17:00:04 +0630

    impl todo crud with angular

--- rx-js-deep-dive/src/app/app.component.ts
+++ rx-js-deep-dive/src/app/app.component.ts
@@ -1,29 +1,19 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
-import {interval, map} from "rxjs";
+import {Component, computed, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
+import {ObservableDemoComponent} from "./obserable-demo/observable-demo.component";
+import {DataTransformationComponent} from "./data-transformation/data-transformation.component";
+import {HigherOrderMappingComponent} from "./higher-order-maping/higher-order-mapping.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    ObservableDemoComponent,
+    DataTransformationComponent,
+    HigherOrderMappingComponent
+  ],
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit{
-  private destroyRef = inject(DestroyRef)
-
-    ngOnInit(): void {
-       // const subscription = interval(1000).pipe(
-       //   map(value => value*2)
-       // ).subscribe({
-       //    next: (value)=>{
-       //      console.log("Value : ",value);
-       //    },
-       //   complete: () =>{
-       //      console.log("Value : complete");
-       //   }
-       //  });
-       //
-       //  this.destroyRef.onDestroy(() =>{
-       //    subscription.unsubscribe();
-       //  });
-    }
+export class AppComponent{
 
 }
